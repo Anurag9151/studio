468bea10eb6cca7ad957db7b6b205bf16cd5d54c
Anurag Maurya <anuragmaurya998877@bbdnitm.ac.in>
2025-09-12 19:18:19 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,16 @@ import { Suspense } from 'react';
 import SubjectWiseAttendance from "./components/subject-wise-attendance";
 import { Skeleton } from '@/components/ui/skeleton';
 import AttendanceSummary from './components/attendance-summary';
+import TodaySchedule from './components/today-schedule';
 
 export default function DashboardPage() {
   return (
     <div className="space-y-6">
       <Suspense fallback={<Skeleton className="h-24 w-full rounded-lg" />}>
         <AttendanceSummary />
+      </Suspense>
+       <Suspense fallback={<Skeleton className="h-64 w-full" />}>
+        <TodaySchedule />
       </Suspense>
       <Suspense fallback={<Skeleton className="h-40 w-full rounded-lg" />}>
         <SubjectWiseAttendance />
