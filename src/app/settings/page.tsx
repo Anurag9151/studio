8e6e3f7d39c398b90a7fec2aae44a30d2a7635bc
Anurag Maurya <anuragmaurya998877@bbdnitm.ac.in>
2025-09-19 18:27:22 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,4 +1,7 @@
-import SettingsForm from "./components/settings-form";
+
+import dynamic from 'next/dynamic';
+
+const SettingsForm = dynamic(() => import('./components/settings-form'), { ssr: false });
 
 export default function SettingsPage() {
   return (
