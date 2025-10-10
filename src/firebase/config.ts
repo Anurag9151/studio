16eee186ef29883c4a8c09ba26b7493fef638de6
Anurag Maurya <anuragmaurya998877@bbdnitm.ac.in>
2025-10-10 08:11:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8290817659-98d2e",
+  "appId": "1:360528934098:web:3527b73a259a512930290b",
+  "apiKey": "AIzaSyAhQapODZUBLoxDlL8GUpmt3HnDHUnCzQ4",
+  "authDomain": "studio-8290817659-98d2e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "360528934098"
+};
